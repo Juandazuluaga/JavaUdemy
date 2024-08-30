f5925ced7a386eb2aa56aed1824e3a437cd415cd
Juandazuluaga <Juandazuluagagiraldo@gmail.com>
2024-08-30 16:16:51 -0500

    for loop

--- src/PruegaGit.java
+++ src/PruegaGit.java
@@ -1,5 +1,8 @@
 public class PruegaGit {
     public static void main(String[] args) {
-        for( int i = 0; i <= 5; i++){}
+        for( int i = 0; i <= 5; i++){
+            System.out.println("si");
+            i++;
+        }
     }
 }
